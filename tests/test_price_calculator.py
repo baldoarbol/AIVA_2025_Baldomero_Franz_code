feb17edfd90355caf38515a024df0a3f033890f3
Baldo <baldorodriguezarbol@gmail.com>
2025-03-11 18:18:48 +0100

    Implementado test sobre el cálculo de precio de los componentes
    Test que comprueba que el cálculo es correcto.
    Tarea asociada #8

--- tests/test_price_calculator.py
+++ tests/test_price_calculator.py
@@ -5,4 +5,4 @@ from AIVA_2025_Baldomero_Franz_code.components.price_calculator import compute_p
 
 class Test(TestCase):
     def test_compute_price(self):
-        TestCase.assertEqual(self, compute_price(3, 6), 9)
+        TestCase.assertEqual(self, compute_price(num_big_t=10, num_small_t=10, price_small_t=0.1, price_big_t=0.9), 10)

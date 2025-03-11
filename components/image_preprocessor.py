465770561442c57ce3059169d7e10a4e70825788
Baldo <baldorodriguezarbol@gmail.com>
2025-03-11 17:31:08 +0100

    Implementado mockup de image preprocessor
    Este mockup recibe como entrada una imagen y devuelve la misma imagen en escala de grises.
    Tarea asociada: #1

--- components/image_preprocessor.py
+++ components/image_preprocessor.py
@@ -2,8 +2,16 @@
 Script con las funciones de preprocesamiento de imagen
 """
 import cv2
+import numpy as np
 
 
-def process_image(img: any) -> str:
-    result = "Imagen procesada"
+def process_image(image: np.ndarray) -> np.ndarray:
+    """
+    Procesa una imagen con una serie de transformaciones.
+
+    :param image: Imagen de entrada en formato numpy.ndarray.
+    :return: Imagen preprocesada en formato numpy.ndarray.
+    """
+
+    result = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     return result

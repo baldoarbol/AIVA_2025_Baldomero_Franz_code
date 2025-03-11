6670f0b922b6ba59e9c07b6b12c05254a8d7f68b
Baldo <baldorodriguezarbol@gmail.com>
2025-03-11 17:16:33 +0100

    Modificada estructura mockup
    Editada la estructura del mockup para la simulación del flujo completo del sistema.
    Tarea asociada #1

--- components/image_preprocessor.py
+++ components/image_preprocessor.py
@@ -1,8 +1,9 @@
 """
 Script con las funciones de preprocesamiento de imagen
 """
+import cv2
 
 
-def process_image() -> str:
+def process_image(img: any) -> str:
     result = "Imagen procesada"
     return result
--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 """
 Script principal del sistema
 """
+import cv2
 
-import components.image_preprocessor
-import components.transistor_detector
-import components.price_calculator
+import components.image_preprocessor as preprocessor
+import components.transistor_detector as detector
+import components.price_calculator as calculator
+
+input_directory = "./test_img/"
+image_name = "im01.png"
+
+raw_img = cv2.imread(input_directory + image_name, cv2.IMREAD_COLOR)
+
+preprocessed_img = preprocessor.process_image(raw_img)
+
+num_small_t = detector.detect_transistor(t_type="small")
+num_big_t = detector.detect_transistor(t_type="big")
+
+total_price = calculator.compute_price(num_small_t, num_big_t)
+
+print(f"PRECIO TOTAL: {total_price}")
+
+

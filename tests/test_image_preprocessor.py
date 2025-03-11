7dccc4d8577bcccf06b8bd31cbdb23ec24c4082f
Baldo <baldorodriguezarbol@gmail.com>
2025-03-11 18:16:37 +0100

    Implementados test unitarios sobre el mockup del sistema
    Test que comprueba que las dimensiones de la imagen antes y después del procesado son las mismas
    Tarea asociada #2

--- tests/test_image_preprocessor.py
+++ tests/test_image_preprocessor.py
@@ -1,8 +1,14 @@
 from unittest import TestCase
 
+import cv2
+
 from AIVA_2025_Baldomero_Franz_code.components.image_preprocessor import process_image
 
+input_directory = "../test_img/"
+image_name = "im01.png"
+raw_img = cv2.imread(input_directory + image_name, cv2.IMREAD_COLOR)
 
 class Test(TestCase):
     def test_process_image(self):
-        TestCase.assertEqual(self, process_image("test_img"), "Imagen procesada")
+        TestCase.assertEqual(self, raw_img.shape[0], process_image(raw_img).shape[0])
+        TestCase.assertEqual(self, raw_img.shape[1], process_image(raw_img).shape[1])

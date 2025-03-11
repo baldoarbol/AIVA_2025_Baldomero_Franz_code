7aa783abc28ef21bcbe67d4b86c37604ea30c972
Baldo <baldorodriguezarbol@gmail.com>
2025-03-11 18:23:36 +0100

    Implementar tests sobre el sistema de detección
    Tests que comprueban que los campos "num", "position" y "size" de los bounding boxes son correctos.
    Tarea asociada #5

--- tests/test_transistor_detector.py
+++ tests/test_transistor_detector.py
@@ -1,10 +1,19 @@
 from unittest import TestCase
 
+import cv2
+
 from AIVA_2025_Baldomero_Franz_code.components.transistor_detector import detect_transistors
+
+input_directory = "../test_img/"
+image_name = "im01.png"
+raw_img = cv2.imread(input_directory + image_name, cv2.IMREAD_COLOR)
 
 
 class Test(TestCase):
     def test_detect_transistor(self):
-        TestCase.assertEqual(self, detect_transistors(t_type="big"), 2)
-        TestCase.assertEqual(self, detect_transistors(t_type="small"), 7)
-        TestCase.assertEqual(self, detect_transistors(t_type="incorrect"), 0)
+        small_t, big_t = detect_transistors(raw_img)
+        TestCase.assertNotEqual(self, small_t["num"], None)
+        TestCase.assertNotEqual(self, small_t["position"], None)
+        TestCase.assertNotEqual(self, small_t["size"], None)
+        TestCase.assertEqual(self, small_t["num"], len(small_t["position"]))
+        TestCase.assertEqual(self, small_t["num"], len(small_t["size"]))
